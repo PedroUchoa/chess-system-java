e917aba8368db086818dd7f11b4a65c520ffbdb1
Joao Pedro <joaopedrouchoacampos@gmail.com>
2022-12-15 12:07:09 -0300

    Special move - Promotion

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -66,7 +66,7 @@ public class Pawn extends ChessPiece {
             if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
                 mat[p.getRow()][p.getCollumn()] = true;
             }
-            p.setValues(position.getRow() + 1, position.getCollumn() + 1);
+            p.setValues(position.getRow() + 1, position.getCollumn() - 1);
             if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
                 mat[p.getRow()][p.getCollumn()] = true;
             }
